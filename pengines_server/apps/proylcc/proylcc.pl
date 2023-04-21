0f801c2f7cc37a8c012b031f7fb7ee5a0cb20a16
Jordan Carranza <jordancarranza045@gmail.com>
2023-04-21 13:58:02 -0300

    Hecha suma y eliminacion

--- pengines_server/apps/proylcc/proylcc.pl
+++ pengines_server/apps/proylcc/proylcc.pl
@@ -15,30 +15,36 @@ replace([_|T], 0, X,[X|T]).
 replace([H|T], I, X, [H|R]):- I > -1, NI is I-1, replace(T, NI, X, R), !.
 replace(L, _, _, L).
 
-numi([X|Y],RIndex) :- RIndex = (X*5)+Y.
+numi([X|[Y]],RIndex) :- RIndex is (X*5)+Y.
 
-% set_all_cero(Grid,Path,RGrids)
-set_all_cero(Grid,[],Grid,456).
+celda(Grid,[X|[Y]],Celda) :- %EXTRAE CELDA
+	numi([X|[Y]],RIndex),
+	nth0(RIndex,Grid,Celda).
 
+% set_all_cero(Grid,Path,RGrids) // podria no estar
+set_all_cero(Grid,[],Grid,SumaTotal).
+
+%eliminar este caso para hacerlo aparte
 set_all_cero(Grid,[P],RGrid,SumaTotal) :-
 	numi(P,RIndex),
-%	nth0(RIndex,Grid,Suma),
-%	SumaTotal = SumaTotal + Suma,
-	replace(Grid, RIndex, SumaTotal, Aux),
-	set_all_cero(Aux,[],RGrid,SumaTotal).
+	celda(Grid,P,Valor),
+	SumaTotalAux is SumaTotal + Valor,
+	replace(Grid, RIndex, SumaTotalAux, Aux),
+	set_all_cero(Aux,[],RGrid,SumaTotalAux).
 
 set_all_cero(Grid,[P|Ps],RGrid,SumaTotal):-
 	numi(P,RIndex),
-%	nth0(RIndex,Grid,Suma),
-%	SumaTotal = SumaTotal + Suma,
+	celda(Grid,P,Valor),
 	replace(Grid, RIndex, 0, Aux),
-	set_all_cero(Aux,Ps,RGrid,SumaTotal).
+	SumaTotalAux is SumaTotal + Valor,
+	set_all_cero(Aux,Ps,RGrid,SumaTotalAux).
 
 
 join(Grid, _NumOfColumns, Path, RGrids):-
-	set_all_cero(Grid,Path,Aux,SumaTotal),
+	set_all_cero(Grid,Path,Aux,0),
 	RGrids = [Aux,Aux].
 
+%random/3
 
 
 
